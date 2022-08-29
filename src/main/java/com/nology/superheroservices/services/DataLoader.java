7be773f737f9f6482c4cfdd6f79989cf3095d838
Sean Pursani <seanpursani@gmail.com>
2022-08-29 19:30:21 +0100

    updated to Marvel api seed characters

--- src/main/java/com/nology/superheroservices/services/DataLoader.java
+++ src/main/java/com/nology/superheroservices/services/DataLoader.java
@@ -1,9 +1,10 @@
 package com.nology.superheroservices.services;
-
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.nology.superheroservices.models.Powerstats;
 import com.nology.superheroservices.models.Superhero;
 import com.nology.superheroservices.repositories.SuperheroRepository;
+import com.nology.superheroservices.util.SuperheroUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -16,9 +17,10 @@ import javax.naming.ServiceUnavailableException;
 public class DataLoader implements ApplicationRunner {
 
     private final SuperheroRepository superheroRepository;
-    @Value("${superheroApiPublicKey}")
-    private String apiKey;
-
+    @Value("${marvelApiPublicKey}")
+    public String publicKey;
+    @Value("${marvelApiPrivateKey}")
+    public String privateKey;
 
     @Autowired
     public DataLoader(SuperheroRepository superheroRepository) {
@@ -28,17 +30,51 @@ public class DataLoader implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         try {
-            this.seedSuperheroes();
+            for (int i = 97; i < 122; i++) {
+                this.seedSuperheroes((char) i);
+            }
         } catch (Exception e) {
             throw new ServiceUnavailableException("Unable to seed superheroes. Exiting program...");
         }
     }
 
-    private void seedSuperheroes() throws Exception {
+    private void seedSuperheroes(char startsWith) throws Exception {
         try {
-            Powerstats powerstats = new Powerstats(75, 34, 87, 34, 67, 56);
-            Superhero superhero = new Superhero("Evil Deadpool", "https://www.superherodb.com/pictures2/portraits/10/100/10090.jpg", powerstats);
-            superheroRepository.save(superhero);
+            String location = "https://gateway.marvel.com/v1/public/characters?"
+                    + "nameStartsWith=" + startsWith + "&ts="
+                    + SuperheroUtil.timeStamp + "&apikey="
+                    + publicKey + "&hash="
+                    + SuperheroUtil.MD5hash(publicKey, privateKey, SuperheroUtil.timeStamp);
+
+            JsonObject rootObj = JsonWebService.getJsonObjectFromUrl(location);
+            JsonObject title = rootObj.get("data").getAsJsonObject();
+            JsonArray results = title.get("results").getAsJsonArray();
+
+            results.forEach(c -> {
+                JsonObject characterJson = c.getAsJsonObject();
+
+                JsonObject tJson = characterJson.get("thumbnail").getAsJsonObject();
+                String image = JsonMapperService.getStringFromJson(tJson, "path")
+                        + "." + JsonMapperService.getStringFromJson(tJson, "extension");
+
+                Powerstats powerstats = new Powerstats(
+                        SuperheroUtil.getRandomNumber(40, 100),
+                        SuperheroUtil.getRandomNumber(40, 100),
+                        SuperheroUtil.getRandomNumber(40, 100),
+                        SuperheroUtil.getRandomNumber(40, 100),
+                        SuperheroUtil.getRandomNumber(40, 100),
+                        SuperheroUtil.getRandomNumber(40, 100)
+                );
+
+                Superhero superhero = new Superhero(
+                        JsonMapperService.getStringFromJson(characterJson, "name"),
+                        image,
+                        powerstats
+                );
+
+                superheroRepository.save(superhero);
+            });
+
         } catch (Exception e) {
             throw new Exception("Error seeding characters from Marvel API.");
         }
